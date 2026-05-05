7fcb22bd6d875eb2de6390b7588cbb10386f25e2
Bobby Maher <bobbymhr@gmail.com>
2026-05-05 12:08:19 +0300

    spx_reporter_full: drop profile when wall time < threshold

--- src/spx_reporter_full.c
+++ src/spx_reporter_full.c
@@ -347,6 +347,14 @@ static void finalize(full_reporter_t * reporter, const spx_profiler_event_t * ev
         reporter->metadata->enabled_metrics[i] = event->enabled_metrics[i];
     });
 
+    if (reporter->drop_under_ms > 0
+        && reporter->metadata->wall_time_ms < reporter->drop_under_ms) {
+        spx_output_stream_close(reporter->output);
+        reporter->output = NULL;
+        unlink(reporter->profile_file_name);
+        return;
+    }
+
     metadata_save(reporter->metadata, reporter->metadata_file_name);
 }
 
